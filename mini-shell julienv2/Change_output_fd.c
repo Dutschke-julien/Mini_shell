470909ab9ac4c0e13773b204d0c2c38bdde59347
Julien Dutschke <jdutschk@k1r2p13.42mulhouse.fr>
2022-10-25 16:34:50 +0200

    maj box julien

--- mini-shell julienv2/Change_output_fd.c
+++ mini-shell julienv2/Change_output_fd.c
@@ -6,7 +6,7 @@
 /*   By: jdutschk <jdutschk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 15:13:16 by jdutschk          #+#    #+#             */
-/*   Updated: 2022/10/20 14:40:14 by jdutschk         ###   ########.fr       */
+/*   Updated: 2022/10/25 16:31:22 by jdutschk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ int	change_output_fd(char **tab)
 {
 	int	i;
 	int	fd;
-	int	old_fd;
 
 	i = 0;
 	fd = 1;
@@ -24,10 +23,9 @@ int	change_output_fd(char **tab)
 	{
 		if (is_exit(tab[i]))
 		{
-			old_fd = fd;
 			fd = open(&tab[i][1], O_RDWR | O_CREAT | O_TRUNC, 0777);
-			if (fd != 1 && old_fd != 1)
-				close(old_fd);
+			if (fd == -1)
+				printf("error\n");
 			delete_case(tab, i);
 		}
 		else
